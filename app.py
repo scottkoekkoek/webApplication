24023bc8db3752549222d9880c26cdef95d4d996
scottkoekkoek <45495343+scottkoekkoek@users.noreply.github.com>
2023-07-07 13:41:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 from weather import main as get_weather
 from weather import sensor_data
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
 
 app = Flask(__name__)
 
 
-
-app = Flask(__name__)
-
-
-
+# the app route for the http request used the function get or post
 @app.route('/', methods=['GET', 'POST'])
+# request from the form the variables cityname, statename and countryname
 def index():
     data = None
     sensor = None
     if request.method == 'POST':
+        # Get the city, state, and country from the form
         city = request.form['cityName']
         state = request.form['stateName']
         country = request.form['countryName']
+        # Get the weather data for the specified location
         data = get_weather(city, state, country)
+        # Get the sensor data
         sensor = sensor_data()
     return render_template('index.html', data=data, data2=sensor)
 
